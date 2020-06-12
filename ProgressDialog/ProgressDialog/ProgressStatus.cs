4e4b02b8156366b8d879f6684e5155179572c6e9
Alexander Meyer <alexander.meyer@ilt.fraunhofer.de>
2020-06-12 13:34:15 +0200

    Added IsFinished and IsCancelled properties to ProgressStatus

--- ProgressDialog/ProgressDialog/ProgressStatus.cs
+++ ProgressDialog/ProgressDialog/ProgressStatus.cs
@@ -10,6 +10,7 @@ namespace ProgressDialog
     {
         private int progressPercent = 0;
         private string message = "Waiting for task to start...";
+        private bool isFinished;
 
         /// <summary>Gets CancellationTokenSource to use to cancel the async function.</summary>
         private CancellationTokenSource CTS { get; set; } = new CancellationTokenSource();
@@ -17,7 +18,23 @@ namespace ProgressDialog
         public CancellationToken CT => CTS.Token;
 
         /// <summary>Command executed when cancel button is clicked.</summary>
-        public DelegateCommand CancelCommand => new DelegateCommand(CTS.Cancel);
+        public DelegateCommand CancelCommand => new DelegateCommand(Cancel);
+
+        /// <summary>Gets a value indicating whether the associated task was cancelled.</summary>
+        public bool IsCancelled => CTS.IsCancellationRequested;
+
+        /// <summary>Gets or sets a value indicating whether the associated taks is finished.</summary>
+        public bool IsFinished
+        {
+            get => isFinished;
+            set
+            {
+                isFinished = value;
+                progressPercent = 100;
+                RaisePropertyChanged(nameof(IsFinished));
+                RaisePropertyChanged(nameof(ProgressPercent));
+            }
+        }
 
         /// <summary>Gets message to be displayed in ProgressDialog.</summary>
         public string Message
@@ -37,6 +54,16 @@ namespace ProgressDialog
             private set
             {
                 progressPercent = value;
+                if (progressPercent >= 100)
+                {
+                    isFinished = true;
+                    RaisePropertyChanged(nameof(IsFinished));
+                }
+                else if (isFinished == true)
+                {
+                    isFinished = false;
+                    RaisePropertyChanged(nameof(IsFinished));
+                }
                 RaisePropertyChanged(nameof(ProgressPercent));
             }
         }
@@ -48,6 +75,12 @@ namespace ProgressDialog
         {
             Message = message;
             ProgressPercent = progressPercent;
+        }
+
+        private void Cancel()
+        {
+            CTS.Cancel();
+            RaisePropertyChanged(nameof(IsCancelled));
         }
     }
 }
